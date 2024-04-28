0712f56aa08631097c30a0cc503b69e6a75b8796
Chibuzo <chibuzo.henry@gmail.com>
2024-04-28 12:35:51 +0100

    debug

--- services/peopleService.js
+++ services/peopleService.js
@@ -81,7 +81,7 @@ const fetchPersonData = async (idNumber) => {
             });
 
             const data = JSON.parse(res.data);
-            console.log(data)
+            console.log(idNumber)
             if (data.Status !== 'Person Not Found') return data;
             return {};
         } catch (err) {
@@ -195,7 +195,7 @@ const verifyNewRecords = async () => {
     console.log(`NID found records: ${fetchedData.length}`);
 
     fetchedData.forEach(async data => {
-        console.log({ data })
+        // console.log({ data })
         let n = 0;
         if (Object.keys(data).length === 0) return;
         // save to a temp table
